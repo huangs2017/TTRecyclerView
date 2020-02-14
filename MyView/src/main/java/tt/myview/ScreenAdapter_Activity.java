38dd601199ba84cb48bc3a5cd0501c9ce5b8104a
huangsong <2455230388@qq.com>
2020-02-14 16:01:05 +0800

    Initial commit

---
+++ MyView/src/main/java/tt/myview/ScreenAdapter_Activity.java
@@ -0,0 +1,16 @@
+package tt.myview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// 屏幕适配
+public class ScreenAdapter_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_adapter_activity);
+    }
+
+}
